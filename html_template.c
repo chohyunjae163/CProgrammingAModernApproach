67b1d4bda739d09463af36aa5dfbf6cde4e0e133
Hyun Jae Cho <50690938+chohyunjae163@users.noreply.github.com>
2022-10-05 01:30:04 +0900

    Update html_template.c

--- html_template.c
+++ html_template.c
@@ -19,6 +19,11 @@ extern int errno ;
       <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n \
       <title>my devlog</title>\n \
       <style>\n \
+	  	.code { \n \
+	   	  background-color: #666666;\n \
+	   	  border-radius: 25px;\n \
+	   	  padding: 20px;\n \
+		} \n \
         a:link { \n \
           color: #F8F8F8;\n \
       	}\n \
@@ -66,11 +71,11 @@ int main(int argc, char** argv)
 	}
 	fclose(file);
 	content[index] = '\0';
-	file = fopen("out.html","w");
+	
 	char buffer[BUFF_SIZE];
 	const char* wrapped_content = wrap_content(buffer,content);
-	printf("%s", wrapped_content);
-	
+
+	file = fopen("out.html","w");
 	fputs(wrapped_content,file);
 	fclose(file);
 	return 0;
